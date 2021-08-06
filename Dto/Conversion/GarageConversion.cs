afd71502be73971dd0b9912527abe9d50bdf8c40
Aclark0 <mert5708@gmail.com>
2021-08-06 11:05:25 +0300

    Added Dto and DtoConversions and implemented.

---
+++ Dto/Conversion/GarageConversion.cs
@@ -0,0 +1,36 @@
+﻿using Dto.Dto;
+using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto.Conversion
+{
+    public static class GarageConversion
+    {
+        public static Garage ToEntity(GarageDto garage)
+        {
+            return new Garage
+            {
+                Capacity = garage.Capacity,
+                CarCount = garage.CarCount,
+                GarageId = garage.GarageId,
+                HotelId = garage.HotelId,
+            };
+        }
+
+        public static GarageDto ToApi(Garage garage)
+        {
+            return new GarageDto
+            {
+                Capacity = garage.Capacity,
+                CarCount = garage.CarCount,
+                GarageId = garage.GarageId,
+                HotelId = garage.HotelId,
+            };
+        }
+
+    }
+}
---
+++ Dto/Dto/HotelDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto.Dto
+{
+    public class HotelDto
+    {
+        public int HotelId { get; set; }
+
+        public string HotelName { get; set; }
+
+        public string City { get; set; }
+    }
+}
